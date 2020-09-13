ee628992b622cd04d25ad67317974024bb043ee0
Eduardo Espino <eduardo.espino@tfja.edu.mx>
2020-09-12 19:28:49 -0500

    Project structure

--- src/main/java/io/maya/platzimarket/web/controller/HelloController.java
+++ src/main/java/io/maya/platzimarket/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package io.maya.platzimarket;
+package io.maya.platzimarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
