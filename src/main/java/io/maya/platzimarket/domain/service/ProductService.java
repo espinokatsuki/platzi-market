e89f1f09045aa8844d374ad30e5ffcf915a855c5
Lalo Espino <edespinom@gmail.com>
2020-09-15 18:56:10 -0500

    @Service implementation

---
+++ src/main/java/io/maya/platzimarket/domain/service/ProductService.java
@@ -0,0 +1,43 @@
+package io.maya.platzimarket.domain.service;
+
+import io.maya.platzimarket.domain.ProductDomain;
+import io.maya.platzimarket.domain.repository.ProductDomainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductDomainRepository productDomainRepository;
+
+    public List<ProductDomain> getAll() {
+        return productDomainRepository.getAll();
+    }
+
+    public Optional<List<ProductDomain>> getByCategory(Integer id) {
+        return productDomainRepository.getByCategory(id);
+    }
+
+    public Optional<List<ProductDomain>> getScarceProducts(Integer minStock) {
+        return productDomainRepository.getScarceProducts(minStock);
+    }
+
+    public Optional<ProductDomain> getProduct(Integer id) {
+        return productDomainRepository.getProduct(id);
+    }
+
+    public ProductDomain save(ProductDomain productDomain) {
+        return productDomainRepository.save(productDomain);
+    }
+
+    public boolean delete(Integer id) {
+        return getProduct(id).map(productDomain -> {
+            productDomainRepository.delete(productDomain.getId());
+            return true;
+        }).orElse(false);
+    }
+
+}
