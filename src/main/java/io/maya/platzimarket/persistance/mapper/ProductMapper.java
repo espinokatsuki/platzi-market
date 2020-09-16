4a2ed3b7cdf320764e1ae71d8a3869dc77e12590
Lalo Espino <edespinom@gmail.com>
2020-09-15 19:18:30 -0500

    @RestController implementation

--- src/main/java/io/maya/platzimarket/persistance/mapper/ProductMapper.java
+++ src/main/java/io/maya/platzimarket/persistance/mapper/ProductMapper.java
@@ -2,7 +2,6 @@ package io.maya.platzimarket.persistance.mapper;
 
 import io.maya.platzimarket.domain.ProductDomain;
 import io.maya.platzimarket.persistance.entity.Product;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -14,19 +13,19 @@ public interface ProductMapper {
     @Mappings({
             @Mapping(source = "id", target = "id"),
             @Mapping(source = "name", target = "name"),
+            @Mapping(source = "idCategory", target = "categoryId"),
+            @Mapping(source = "barCode", target = ""),
             @Mapping(source = "price", target = "price"),
             @Mapping(source = "stock", target = "stock"),
-            @Mapping(source = "status", target = "active"),
-            @Mapping(source = "category", target = "category"),
-            @Mapping(source = "categoryId", target = "idCategory")
+            @Mapping(source = "state", target = "active"),
+            @Mapping(source = "category", target = "category")
     })
+    ProductDomain toProductDomain(Product product);
+
+    List<ProductDomain> toProductsDomain(List<Product> products);
+
+    @Mapping(target = "barCode", ignore = true)
     Product toProduct(ProductDomain productDomain);
 
     List<Product> toProducts(List<ProductDomain> productDomains);
-
-    @InheritInverseConfiguration
-    @Mapping(target = "barCode", ignore = true)
-    ProductDomain toProductDomain(Product product);
-
-    List<ProductDomain> toProductsDomain(List<Product> product);
 }
