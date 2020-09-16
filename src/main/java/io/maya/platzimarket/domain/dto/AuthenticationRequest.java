e0312908725b177fe862ab66fb715e6355c9f1ec
Lalo Espino <edespinom@gmail.com>
2020-09-16 17:37:11 -0500

    JWT generation

---
+++ src/main/java/io/maya/platzimarket/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package io.maya.platzimarket.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/io/maya/platzimarket/domain/dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package io.maya.platzimarket.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
